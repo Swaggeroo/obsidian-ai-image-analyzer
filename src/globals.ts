cc44b49cd80c5d9232c54bf963505bc0376e22af
Swaggeroo <mugel849@gmail.com>
2024-12-22 17:20:02 +0100

    :sparkles: New model llama3.2-vision

--- src/globals.ts
+++ src/globals.ts
@@ -6,6 +6,8 @@ export const libVersion = packageJson.version;
 
 export const possibleModels: Model[] = [
 	{name: 'llava-llama3 (8B) [default]', model: 'llava-llama3:latest'},
+	{name: 'llama3.2-vision (11B)', model: 'llama3.2-vision:11b'},
+	{name: 'llama3.2-vision (90B)', model: 'llama3.2-vision:90b'},
 	{name: 'llava (7B)', model: 'llava:latest'},
 	{name: 'llava (13B)', model: 'llava:13b'},
 	{name: 'llava (34B)', model: 'llava:34b'}
